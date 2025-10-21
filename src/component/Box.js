0f4b5a167c2202463b732bd5f83ca66badbd3a93
Panda-racoon <millie3519@gmail.com>
2025-10-22 01:42:29 +0900

    fix(a11y): add alt to img in Box.js

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,7 +5,11 @@ const Box = (props) => {
   return (
     <div className="box">
       <h1>{props.title}</h1>
-      <img className="item-img" src={props.item && props.item.img} />
+      <img
+        className="item-img"
+        src={props.item && props.item.img}
+        alt="가위바위보"
+      />
       <h2>WIN</h2>
     </div>
   );
