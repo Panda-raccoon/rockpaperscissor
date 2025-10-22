950d69cb60b0fc7da3e07c1b023d87ff24842c60
Panda-racoon <millie3519@gmail.com>
2025-10-23 01:23:01 +0900

    결과값 화면에 나오게 하기

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const choice = {
   },
   scissors: {
     name: "scissors",
-    img: "https://i.pinimg.com/1200x/83/9e/c1/839ec1bf4755a09c52fdf4914588c289.jpg",
+    img: "https://i.pinimg.com/736x/49/bd/d3/49bdd36a81cdb285e748ba467678431b.jpg",
   },
   paper: {
     name: "paper",
@@ -25,18 +25,53 @@ const choice = {
 };
 
 function App() {
-  const [userSelect, setUserSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null); //유저가 선택한 값
+  const [computerSelect, setComputerSelect] = useState(null); //컴퓨터가 선택한 값
+  const [result, setResult] = useState(""); // 승패 결과
 
   const play = (userChoice) => {
-    // console.log("play", userChoice);s
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice)); //judgement(유저가 선택한 값, 컴퓨터가 선택한 값) judgement뜻 = 판단
+  };
+
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    // user === computer 비김
+    // user === rock , computer === scissors  유저이김
+    // user === scissors , computer === paper  유저이김
+    // user === paper , computer === rock  유저이김
+    // 나머지 졌음
+
+    // 삼항연산식*************** (공부)
+    // 유저입장
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "rock")
+      return computer.name === "scissors" ? "win" : "lose";
+    else if (user.name === "scissors")
+      return computer.name === "paper" ? "win" : "lose";
+    else if (user.name === "paper")
+      return computer.name === "rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); // 객체의 ‘key(이름)’만 모아서 배열로 만들어주는 함수
+    console.log("itemArray", itemArray);
+    // let randomItem = Math.random() * itemArray.length; // Math.random() : 0 이상 1 미만의 실수(소수점이 있는 숫자) 를 무작위로 만들어주는 함수
+    let randomItem = Math.floor(Math.random() * itemArray.length); // Math.floor() : 소수점 이하를 버리는 함수
+    console.log("random", randomItem); // floor 때문에 0,1,2 중 하나의 정수가 나옴
+    let final = itemArray[randomItem];
+    return choice[final];
   };
 
   return (
     <div>
       <div className="main">
-        <Box title="You" item={userSelect} />
-        {/* <Box title="computer" /> */}
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,16 +1,30 @@
 import React from "react";
 
 const Box = (props) => {
-  console.log("props", props);
+  const initialImage =
+    "https://i.pinimg.com/736x/cd/bb/25/cdbb2598bdae322e2dba7c49f838ff10.jpg";
+
+  let result;
+  if (
+    props.title === "computer" &&
+    props.result !== "tie" && // !== : 같지않다
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
   return (
     <div className="box">
       <h1>{props.title}</h1>
       <img
         className="item-img"
-        src={props.item && props.item.img}
+        // src={props.item && props.item.img}
+        src={props.item ? props.item.img : initialImage}
         alt="가위바위보"
       />
-      <h2>WIN</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
